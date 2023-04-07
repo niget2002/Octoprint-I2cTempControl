3d30e71faeab28df0c6ff36aca8655ce6cced68d
niget2002 <david@wallings.net>
2023-04-07 12:48:34 -0500

    add color

--- octoprint_i2ctempcontrol/static/js/i2ctempcontrol.js
+++ octoprint_i2ctempcontrol/static/js/i2ctempcontrol.js
@@ -15,17 +15,44 @@ $(function() {
         self.controlState = ko.observable("Off");
         self.setTempMin = ko.observable(0);
         self.setTempMax = ko.observable(0);
+        self.heaterColor = ko.observable("red");
+        self.fanColor = ko.observable("red");
+        self.controlColor = ko.observable("red");
 
         self.onDataUpdaterPluginMessage = function (plugin, data) {
-            if (data.temperatureValue) { self.temperatureValue(data.temperatureValue); }
-            if (data.setTempMin) {self.setTempMin(data.setTempMin)}
-            if (data.setTempMax) {self.setTempMax(data.setTempMax)}
-            if (data.controlState == 1) { self.controlState("On");}
-            else {self.controlState("Off")}       
-            if (data.fanState == 1) { self.fanState("On") }
-            else {self.fanState("Off") }        
-            if (data.heaterState == 1) { self.heaterState("On") }
-            else { self.heaterState("Off") }     
+            if (data.temperatureValue) { 
+                self.temperatureValue(data.temperatureValue); 
+            }
+            if (data.setTempMin) {
+                self.setTempMin(data.setTempMin)
+            }
+            if (data.setTempMax) {
+                self.setTempMax(data.setTempMax)
+            }
+            if (data.controlState == 1) { 
+                self.controlState("On");
+                self.controlColor("green");
+            }
+            else {
+                self.controlState("Off")
+                self.controlColor("red");
+            }       
+            if (data.fanState == 1) {
+                self.fanState("On")
+                self.fanColor("green");
+            }
+            else {
+                self.fanState("Off")
+                self.fanColor("red");
+            }        
+            if (data.heaterState == 1) {
+                self.heaterState("On")
+                self.heaterColor("green");
+            }
+            else {
+                self.heaterState("Off")
+                self.heaterColor("red");
+            }     
         }
 
         self.startLoop = function() {
